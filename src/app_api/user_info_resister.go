d0ed44eb1730513677d6e3b25c7986b6fee65d67
backspace229 <backspace229@gmail.com>
2017-05-10 00:48:42 +0900

    issue/1 POST内のJSONをパースした

--- src/app_api/user_info_resister.go
+++ src/app_api/user_info_resister.go
@@ -2,6 +2,10 @@ package app_api
 
 import (
 	"net/http"
+
+	"io/ioutil"
+
+	"encoding/json"
 	"src/logic"
 
 	"google.golang.org/appengine"
@@ -13,9 +17,23 @@ func UserInfoResister(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
 
 	//リクエストからパラメータを取得する
-	//vars := mux.Vars(r)
+	bytestr, _ := ioutil.ReadAll(r.Body)
+	body := string(bytestr)
 
-	log.Infof(c, "[r.Body] %v", r.Body)
+	log.Infof(c, "[BODY] %v", body)
 
+	var test TestModel
+
+	err := json.Unmarshal(bytestr, &test)
+	if err != nil {
+		log.Infof(c, "[ERROR] %v", err)
+	}
+
+	log.Infof(c, "[UserID]%v, [Password]%v", test.UserID, test.Password)
 	logic.UserInfoPut()
+}
+
+type TestModel struct {
+	UserID   string `json:"user_id"`
+	Password string `json:"password"`
 }
